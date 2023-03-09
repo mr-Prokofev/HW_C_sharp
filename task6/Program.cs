b0c9d0849da4d06bcd8f506924e2383a9d65ed92
mr Prokofev <prav.1@list.ru>
2023-03-09 22:10:51 +0300

    add task6 Четное/нечетное

---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число А:");
+int numA = Convert.ToInt32(Console.ReadLine());
+if (numA % 2 == 1)
+{
+    Console.WriteLine($"{numA} - нечетное");
+}
+else
+    {
+        Console.WriteLine($"{numA} - четное");
+    }
