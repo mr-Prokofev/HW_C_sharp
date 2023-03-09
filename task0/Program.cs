9c463ec8fc783c021a1fdbabc8d531310092e0e0
mr Prokofev <prav.1@list.ru>
2023-03-09 21:32:04 +0300

    add task0 max(a,b)

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число А:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В:");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Максимальное");
+if (numA > numB)
+    {
+        Console.WriteLine(numA);
+    }
+else
+{
+    Console.WriteLine(numB);
+}
