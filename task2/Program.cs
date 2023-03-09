b49fb703b12a8e26b0790476977ed09e96b85f75
mr Prokofev <prav.1@list.ru>
2023-03-09 22:40:03 +0300

    rename task0 => task2

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число А:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В:");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Максимальное: ");
+if (numA > numB)
+    {
+        Console.Write(numA);
+    }
+else
+{
+    Console.Write(numB);
+}
