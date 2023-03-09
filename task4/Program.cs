bfae691eec44b5183d0b67de13bf3ce72440b531
mr Prokofev <prav.1@list.ru>
2023-03-09 22:04:07 +0300

    add task4 complete

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число А:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В:");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C:");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+int maxNum = Math.Max(numA, Math.Max(numB,numC));
+Console.WriteLine($"Максимальное число: {maxNum}");
