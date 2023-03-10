585ad2e1f486c5ef6a46225681ad3af916c6a34c
mr Prokofev <prav.1@list.ru>
2023-03-10 11:22:31 +0300

    revu task6

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите число А:");
 int numA = Convert.ToInt32(Console.ReadLine());
-if (numA % 2 == 1)
+if (Math.Abs(numA) % 2 == 1)
 {
     Console.WriteLine($"{numA} - нет");
 }
