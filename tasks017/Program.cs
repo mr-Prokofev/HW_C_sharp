3ff50f9d5fa9156afe8c220f5f9ca8bac41b1692
mr Prokofev <prav.1@list.ru>
2023-03-14 21:03:25 +0300

    task017

---
+++ tasks017/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("X");
+int X1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Y");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+
+int Coordinate(int x, int y)
+{
+    if (x>0 && y>0) return 1;
+    if (x<0 && y>0) return 2;
+    if (x<0 && y<0) return 3;
+    if (x>0 && y<0) return 4;
+    return 0;
+}
+
+Console.WriteLine($"Четверь: {Coordinate(X1,Y1)}");
+
+
+
