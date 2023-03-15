2521e770e5cb5feafdadbf486282671a0d75cdfa
mr Prokofev <prav.1@list.ru>
2023-03-15 11:34:33 +0300

    add task019 Palindrome

---
+++ task019/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+bool resultFuncCheckingTheNumber = checkingTheNumber(num) > 0 ? true: false;
+if (resultFuncCheckingTheNumber == true)
+{
+    string result = Palindrome(checkingTheNumber(num)) ? "Да" : "Нет";
+    Console.WriteLine($"{num} --> {result}");
+}
+else
+{
+    Console.WriteLine("Неккоректные данные");
+}
+
+int checkingTheNumber (int a) // Проверка на 5 знаков, преобразование в модуль.
+{
+    int numFunc = Math.Abs(a);
+    if (numFunc > 9999 && numFunc < 100000) return numFunc;
+    return 0;  
+}
+bool Palindrome(int x)  // Проверка на палиндром числа, полученного из функции выше
+{
+    string reverseNum = "";
+    int last = 0;
+    while (x != 0)
+    {
+        last = x%10;
+        reverseNum+=last;
+        x/=10;
+        
+    }
+    bool resultFunc = int.Parse(reverseNum) == checkingTheNumber(num) ? true : false;
+    return resultFunc;
+}
+
+
