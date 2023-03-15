a25efeb96dc8c6058b0298d56a0b029a8b0853ad
mr Prokofev <prav.1@list.ru>
2023-03-15 10:41:58 +0300

    add task022

---
+++ task022/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите предел: ");
+int limit = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i<=limit; i++)
+{
+    Console.WriteLine($"{i, 3} - > {Math.Pow(i,2), 5} ");
+}
