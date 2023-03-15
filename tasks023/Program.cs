eb9cd73677863a7577acefa665a9d3c9b482872f
mr Prokofev <prav.1@list.ru>
2023-03-15 12:05:58 +0300

    add task023 Cube

--- tasks023/Program.cs
+++ tasks023/Program.cs
@@ -4,6 +4,6 @@ int limit = Convert.ToInt32(Console.ReadLine());
 
 for (int i = 1; i<=limit; i++)
 {
-    Console.WriteLine($"{i, 3} - > {Math.Pow(i,3), 5} ");
+    Console.WriteLine($"{i, 3} | {Math.Pow(i,3), 5} ");
 }
 
