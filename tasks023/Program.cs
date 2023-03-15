bd70104b909e19250566895157b0b9d18fee3e4c
mr Prokofev <prav.1@list.ru>
2023-03-15 11:51:14 +0300

    add task023 Cube

---
+++ tasks023/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите предел: ");
+int limit = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i<=limit; i++)
+{
+    Console.WriteLine($"{i, 3} - > {Math.Pow(i,3), 5} ");
+}
+
