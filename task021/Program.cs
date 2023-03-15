a4698e921673aa97c8f5e2ccc1bd48905c36561f
mr Prokofev <prav.1@list.ru>
2023-03-15 11:45:03 +0300

    add task021 Coordinates of points

---
+++ task021/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.WriteLine("Введите x1:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите y1:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите z1:");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите x2:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите y2:");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите z2:");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double distanceAB (int ax, int ay, int az, int bx, int by, int bz)
+{
+    double result = Math.Sqrt(Math.Pow(bx - ax, 2) +  Math.Pow(by-ay,2) +  Math.Pow(bz-az,2));
+    return result;
+}
+
+double resultFuncAndRound = Math.Round(distanceAB(x1, y1, z1, x2, y2, z2), 2, MidpointRounding.ToZero);
+Console.WriteLine($"Расстояние между точками А и В - > {resultFuncAndRound}");
