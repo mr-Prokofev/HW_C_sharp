67ef4627fbddd6f0a2f50a04cdb466d9614aa305
mr Prokofev <prav.1@list.ru>
2023-03-18 13:32:12 +0300

    add task028

---
+++ task028/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int numA = Convert.ToInt32(Console.ReadLine());
+int FactorialNumA(int x)
+{
+    if (x==1) return 1;
+    return x * FactorialNumA(x - 1);
+}
+
+int resultFunc = FactorialNumA(numA);
+Console.WriteLine(resultFunc);
