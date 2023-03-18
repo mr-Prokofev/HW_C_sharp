d76e24cc1e746b96deeb50258d5ed8235de7d921
mr Prokofev <prav.1@list.ru>
2023-03-18 14:35:13 +0300

    add task 029

---
+++ task029/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+int[] array = new int[8];
+
+int[] arrZap(int[] b)
+{
+    Random random = new Random();
+    for (int j = 0; j<b.Length; j++)
+    {
+        b[j] = random.Next(1, 100);
+    }
+    return b;
+}
+
+void printArray(int[] a)
+{
+    for (int i =0; i<a.Length; i++)
+    {
+        Console.Write("{0} ", a[i]);
+    }
+}
+
+printArray(array);
+Console.WriteLine();
+arrZap(array);
+printArray(array);
