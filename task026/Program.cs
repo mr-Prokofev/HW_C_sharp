65db7057efa339e7fbc19db02bf8cc9606451b5a
mr Prokofev <prav.1@list.ru>
2023-03-18 13:23:35 +0300

    add task026

---
+++ task026/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int numA = Convert.ToInt32(Console.ReadLine());
+
+int countNumA (int x)
+{
+    int count = 0;
+    while (x!=0)
+    {
+        count++;
+        x/=10;
+    }
+    return count;
+}
+
+int resFunc = countNumA(numA);
+Console.WriteLine(resFunc);
