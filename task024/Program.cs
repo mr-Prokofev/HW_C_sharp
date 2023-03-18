0aa2675959d6d7f974d92da55a1c6415b9bb1ce2
mr Prokofev <prav.1@list.ru>
2023-03-18 13:17:19 +0300

    add task024

---
+++ task024/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int numA = Convert.ToInt32(Console.ReadLine());
+
+int sumNumA(int x)
+{
+    int simI = 0;
+    for (int i = 1; i<=x; i++)
+    {
+        simI+=i;
+    }
+    return simI;
+}
+
+int resFunc = sumNumA(numA);
+Console.WriteLine(resFunc);
