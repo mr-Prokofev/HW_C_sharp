13031e4ee718c87228d04bb73ce3802f6d0a355c
mr Prokofev <prav.1@list.ru>
2023-03-18 14:18:51 +0300

    add task025

---
+++ task025/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int num = Convert.ToInt32(Console.ReadLine());
+int st = Convert.ToInt32(Console.ReadLine());
+if (st <= 0)
+{
+    Console.WriteLine("Ошибка");
+    return;
+}
+
+void StNum(int a, int b)
+{
+    int step = 1;
+    for (int i = 1; i<=b; i++)
+    {
+        step*=a;
+    }
+    Console.WriteLine($"число {a} в степени {b}: {step}");
+}
+StNum(num, st);
