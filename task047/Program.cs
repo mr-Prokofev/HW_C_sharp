550d27114a0de2e5a6517e9e01e2be64e8c20ed9
mr Prokofev <prav.1@list.ru>
2023-04-01 20:07:16 +0300

    add task047

---
+++ task047/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+double[,] CreateMatrixDouble(int rows, int colums, int min, int max) //заполнение случайными вещественными числами
+{
+    double[,] matrix = new double[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j]= rnd.NextDouble()* (max - min) + min;
+        }   
+    }
+    return matrix;
+}
+void printMatrixRound(double[,] matrix) // вывод округленных значений
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j< matrix.GetLength(1); j++)
+    {
+        Console.Write($"{Math.Round(matrix[i, j], 1), 5}"); 
+    }
+    Console.WriteLine();
+    } 
+}
+
+double[,] array2d = CreateMatrixDouble(3,4, -10, 10);
+printMatrixRound(array2d);
